1009b9b89b9a34d418bdfcfed0778ea04d90a4fd
JK-SHIN-PG <shinjk1156@unist.ac.kr>
2022-03-09 02:14:17 +0900

    model upload

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+#using attention
+class transformer(nn.Module):
+    def __init__(self):
+        super(transformer,self).__init__()
+        pass
+    def forward(self,x):
+        pass
